777a58714837578b02c283fb89b0352179acdf39
giakhiemdt <khiem1371@gmail.com>
2025-08-26 21:24:29 +0700

    Add gateway service.

---
+++ gateway-service/src/main/kotlin/org/example/gatewayservice/GatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package org.example.gatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayServiceApplication>(*args)
+}
